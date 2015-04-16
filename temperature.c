23228a2710d5075ae263940311375bf18ee0c6bc
Jon <JonCGreen12@gmail.com>
2015-04-15 21:01:46 -0500

    Temperature.c now only accepts numbers as input

--- temperature.c
+++ temperature.c
@@ -3,9 +3,12 @@
 int main() {
     printf("Enter a temperature in Fahrenheit:");
     float temp_f;
-    scanf("%f", &temp_f);
-    float temp_c;
-    temp_c = (temp_f - 32) * 5/9;
-    
-    printf("That's %f degrees Celsius.\n", temp_c);
+    if (scanf("%f", &temp_f) != 1) {
+        printf("Read Error, please enter a number.\n");
+    } else {
+        float temp_c;
+        temp_c = (temp_f - 32) * 5/9;
+        //printf("Your input was %f\n", temp_f);
+        printf("That's %f degrees Celsius.\n", temp_c);
+    };
 }
